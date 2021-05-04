80d034a1cd6d2901498031d618e9b85127d39f82
Yusuf Çırak <yusuf10901@hotmail.com>
2021-05-04 22:20:51 +0300

    04.05.2021

--- src/app/components/car-detail/car-detail.component.ts
+++ src/app/components/car-detail/car-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CarDetailDto } from 'src/app/models/carDetailDto';
+import { Car } from 'src/app/models/car';
 import { CarImage } from 'src/app/models/carImage';
 import { CarService } from '../services/car.service';
 import { CarImageService } from '../services/carImage.service';
@@ -11,38 +11,31 @@ import { CarImageService } from '../services/carImage.service';
   styleUrls: ['./car-detail.component.css']
 })
 export class CarDetailComponent implements OnInit {
-  currentCar:CarDetailDto;
-  carImages:CarImage[];
-  dataLoaded=false;
-  constructor(
-    private carService:CarService,
-    private carImageService:CarImageService,
-    private activatedRoute:ActivatedRoute
-  ) { }
+  carImages:CarImage[]=[];
+  car!:Car;
+  defaultPath="https://localhost:44334"
+
+  constructor(private carService:CarService,private carImageService:CarImageService, 
+    private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if (params["carId"]){
-        this.getCarDetail(params["carId"]);
-        this.getCarImages(params["carId"]);
-
-      }
-    })
-    
-  }
-
-  getCarDetail(carId:number){
-    this.carService.getCarDetail(carId).subscribe(response=>{
-      this.currentCar=response.data
-      this.dataLoaded=true;
-    })
   }
 
   getCarImages(carId:number){
     this.carImageService.getImagesByCarId(carId).subscribe(response=>{
-      this.carImages=response.data;
-      this.dataLoaded=true;
+      this.carImages = response.data;
+      console.log(this.carImages);
+      console.log(response)
     })
+  }
+
+  getCarsByCarId(carId:number){
+    this.carService.getCarDetailById(carId).subscribe(response=>{
+      this.car = response.data[0];
+    })
+  }
+  getImagePath(image:string){
+    return this.defaultPath + image;
   }
 
 }
--- src/app/components/services/carImage.service.ts
+++ src/app/components/services/carImage.service.ts
@@ -12,6 +12,11 @@ export class CarImageService {
 
   constructor(private httpClient:HttpClient) { }
 
+  getImages():Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"carimages/getall"
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath)
+  }
+
   getImagesByCarId(carId:number):Observable<ListResponseModel<CarImage>>{
     let newPath=this.apiUrl+"carimages/getbycarid="+carId
     return this.httpClient.get<ListResponseModel<CarImage>>(newPath)
